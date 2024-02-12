b09539948b037160ceaeeaa58e77bfb47c385fa2
nachiket <nachiketjagtap9881@gmail.com>
2024-02-12 10:41:26 +0530

    Used NEW API to fetch recent news

--- src/App.js
+++ src/App.js
@@ -6,25 +6,32 @@ import NewsContent from "./components/NewsContent/NewsContent";
 import Header from "./components/Header/Header";
 
 function App() {
-  const [category, setcategory] = useState("business");
-  const [newsArray, setnewsArray] = useState([]);
+  const [newsArray, setNewsArray] = useState([]);
 
-  useEffect(async () => {
-    try {
-      const getResponse = await axios.get(
-        `https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`
-      );
-      setnewsArray(getResponse.data.articles);
-    } catch (error) {
-      alert(error);
-    }
-  });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://newsapi.org/v2/top-headlines?country=in&apiKey=0b5f016413dd45cdb800cb781669752c"
+        );
+        if (response.status === 200 && response.data.status === "ok") {
+          setNewsArray(response.data.articles);
+        } else {
+          console.error("Error fetching data: ", response.status);
+        }
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <Header setcategory={setcategory}></Header>
-      <NewsContent newsArray={newsArray}></NewsContent>
-      <Footer></Footer>
+      <Header />
+      <NewsContent newsArray={newsArray} />
+      <Footer />
     </div>
   );
 }
